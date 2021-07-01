1a18a2621ae4b5ec37e932a512361a1279185906
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-01 10:56:04 +0100

    Question 9 completed
    Also slightly tweaked Q8

--- q8.py
+++ q8.py
@@ -3,6 +3,6 @@
 class Student:
     def __init__(self, name):
         self.name = name
-        print(self.name)
 
 devops_student = Student("Aman")
+print(devops_student.name)
---
+++ q9.py
@@ -0,0 +1,10 @@
+# Create two functions that take two arguments each. func1 = add_value. func2 = subtract_value. Return both of these values
+
+def add_value(num1, num2):
+    return num1 + num2
+
+def subtract_value(num1, num2):
+    return num1 - num2
+
+print(add_value(1, 2))
+print(subtract_value(1, 2))
