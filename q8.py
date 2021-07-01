265c61f20e51c822d413391891088acf786a40de
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-01 10:51:44 +0100

    Question 8 completed

---
+++ q8.py
@@ -0,0 +1,8 @@
+# Create a class called Student, initialise the class and create an object of the class
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
+
+devops_student = Student("Aman")
