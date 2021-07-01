7d7f6d0709be8e1b48b7754bf419c8f5a6aac18b
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-01 12:17:43 +0100

    Updated Q12

--- q12.py
+++ q12.py
@@ -1,2 +1,3 @@
-# select the correct syntax- 1 -super.__init(). 2- super()__init(). 3 super().__init(). 4 - super().__init__()
-# 4 - super().__init__()
+# select the correct syntax- 1 -super.__init(). 2- super()__init(). 3 super().__init(). 4 - Super().__init__()
+# None of then, it's actually super().__init__()
+# Pay attention to cases
